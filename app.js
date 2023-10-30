16eab0242e828a446866a4198fbb2e8016aad6ef
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-10-30 09:07:34 -0400

    Added some base functionality that generated a random code, as well as finished function that allows player to input a guess and display the accuracy of their guess against the code. Next step is to allow this process to loop for allow for six guesses.

---
+++ app.js
@@ -0,0 +1,115 @@
+console.log('js: loaded')
+
+// constants
+const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white']
+const accColors = ['black', 'white', 'chocolate']
+// state variables
+let code = [];
+let guess =[];
+let guessAcc = [];
+// cached elements
+//start button
+startButtonEl = document.querySelector('#start')
+//color buttons
+redButtonEl = document.querySelector('#red')
+blueButtonEl = document.querySelector('#blue')
+yellowButtonEl = document.querySelector('#yellow')
+greenButtonEl = document.querySelector('#green')
+blackButtonEl = document.querySelector('#black')
+whiteButtonEl = document.querySelector('#white')
+
+// event listeners
+redButtonEl.addEventListener('click', handleClick)
+blueButtonEl.addEventListener('click', handleClick)
+yellowButtonEl.addEventListener('click', handleClick)
+greenButtonEl.addEventListener('click', handleClick)
+blackButtonEl.addEventListener('click', handleClick)
+whiteButtonEl.addEventListener('click', handleClick)
+
+// functions
+function generateCode() { //generates the code to be guessed by the player
+    for(let i=0;i<4;i++) {
+        let codeSequence = Math.floor(Math.random()*colors.length)
+        code.push(codeSequence);
+    }
+}
+
+function gamePinColor(pin, colorIndex, id) { //function to change color of pin
+    let gamePin = document.querySelector(`${id} div:nth-child(${pin})`);
+    if(colors[colorIndex]==='red') {
+        gamePin.style.backgroundColor = "red";
+    } else if(colors[colorIndex]=== 'blue') {
+        gamePin.style.backgroundColor = "blue";
+    } else if(colors[colorIndex]=== 'yellow') {
+        gamePin.style.backgroundColor = "yellow";
+    } else if(colors[colorIndex]=== 'green') {
+        gamePin.style.backgroundColor = "green";
+    } else if(colors[colorIndex]=== 'black') {
+        gamePin.style.backgroundColor = "black";
+    } else if(colors[colorIndex]=== 'white') {
+        gamePin.style.backgroundColor = "white";
+    }
+    // console.log(gamePin)
+}
+
+function accPinColor(pin, colorIndex, id) {
+    let accPin = document.querySelector(`${id} div:nth-child(${pin})`);
+    if(accColors[colorIndex] === 'black') {
+        accPin.style.backgroundColor ='black';
+    } else if(accColors[colorIndex]==='white') {
+        accPin.style.backgroundColor = 'white';
+    } else if(accColors[colorIndex]==='chocolate') {
+        accPin.style.backgroundColor ='chocolate';
+    }
+    console.log(accPin);
+    console.log(accColors[colorIndex]);
+}
+
+function handleClick(e) {
+    if(guess.length<4) {
+        guess.push(colors.indexOf(e.target.id))
+    } 
+    // console.log(e.target)
+    // console.log(guess)
+    render();
+}
+
+function render() {
+    if(guess.length === 4) {
+        guess.forEach((el,i)=> {
+            gamePinColor(i+1,el,'#guess1')
+        })
+
+        console.log(guess)
+        console.log(code)
+
+        checkAccuracy(guess,code);
+        console.log(guessAcc)
+        guessAcc.forEach((el,i) => {
+            accPinColor(i+1,el,'#accuracy1')
+        })
+    }
+
+}
+
+function checkAccuracy(guess, code) {
+    for(let i=0;i<guess.length;i++) {
+        if(guess[i]===code[i]) {
+            guessAcc.push(accColors.indexOf('black'))
+        } else if(code.includes(guess[i])) {
+            guessAcc.push(accColors.indexOf('white'))
+        } 
+        if(guess[i]!==code[i] && code.includes(guess[i])===false) {
+            guessAcc.push(accColors.indexOf('chocolate'))
+        }
+    }
+    guessAcc.sort();
+    // console.log(guessAcc)
+}
+
+generateCode()
+// console.log(code)
+
+code.forEach((el,i) => { //displays the secret code in the answer box. Will move to an if statement so that it only displays in winning or losing conditions.
+    gamePinColor(i+1, el, '#answer');
+})
